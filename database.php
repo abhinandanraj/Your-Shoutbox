2cf92b06350923427aa8711242e02bc7c8bb46b5
Abhinandan Raj <42587988+abhinandanraj@users.noreply.github.com>
2021-07-29 17:04:28 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to MySQL
-$con = mysqli_connect("localhost","root","121212aa","jsshoutbox");
+$con = mysqli_connect("localhost","root","yes","jsshoutbox");
 
 if(mysqli_connect_errno()){
 	echo 'Failed to connect: '.mysqli_connect_error();
